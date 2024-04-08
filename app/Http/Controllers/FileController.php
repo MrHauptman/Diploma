35ead2aeee10ffe2d25819de2839678ba1ea1810
root <root@debian.Domain_pavel>
2024-04-08 09:46:11 +0300

    readable date

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreFolderRequest;
+use App\Http\Resources\FileResource;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use App\Models\File;
@@ -11,7 +12,10 @@ use Illuminate\Support\Facades\Auth;
 
 class FileController extends Controller
 {
-    public function myFiles() {
+    
+    
+    public function myFiles() 
+    {
         $folder= $this->getRoot();
         $files = File::query()
         ->where('parent_id', $folder->id)
@@ -19,8 +23,8 @@ class FileController extends Controller
         ->orderBy('is_folder','desc')
         ->orderBy('created_at','desc')
         ->paginate(10);
+        $files= FileResource::collection($files);    
         
-        ;
         return Inertia::render('MyFiles',compact('files'));
     
 }
@@ -38,6 +42,7 @@ public function createFolder(StoreFolderRequest $request)
     $file->is_folder = 1;
     $file->name = $data['name'];
     $parent->appendNode($file);
+    
 }
 
 private function getRoot()
